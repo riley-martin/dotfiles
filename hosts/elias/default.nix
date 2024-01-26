54903bf8e7eb138a0d3e2162755237240ede3321
Riley Martin <rileyseanm@gmail.com>
2024-01-26 10:38:35 -0500

    .

--- hosts/elias/default.nix
+++ hosts/elias/default.nix
@@ -362,18 +362,18 @@
         forceSSL = true;
         # clientMaxBodySize = "20M";
         locations = {
-          # " = /" = {
-          #   return = "302 https://$host/web";
-          # };
+          " = /" = {
+            return = "302 https://$host/web";
+          };
           "/" = {
             proxyPass = "http://127.0.0.1:8096";
             extraConfig = ''
               proxy_set_header Host $host;
-              proxy_set_header X-Real-IP $remote_addr;
-              proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-              proxy_set_header X-Forwarded-Proto $scheme;
-              proxy_set_header X-Forwarded-Protocol $scheme;
-              proxy_set_header X-Forwarded-Host $http_host;
+              # proxy_set_header X-Real-IP $remote_addr;
+              # proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+              # proxy_set_header X-Forwarded-Proto $scheme;
+              # proxy_set_header X-Forwarded-Protocol $scheme;
+              # proxy_set_header X-Forwarded-Host $http_host;
 
               # Disable buffering when the nginx proxy gets very resource heavy upon streaming
               proxy_buffering off;
@@ -383,11 +383,11 @@
             proxyPass = "http://127.0.0.1:8096/web/index.html";
             extraConfig = ''
               proxy_set_header Host $host;
-              proxy_set_header X-Real-IP $remote_addr;
-              proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-              proxy_set_header X-Forwarded-Proto $scheme;
-              proxy_set_header X-Forwarded-Protocol $scheme;
-              proxy_set_header X-Forwarded-Host $http_host;
+              # proxy_set_header X-Real-IP $remote_addr;
+              # proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+              # proxy_set_header X-Forwarded-Proto $scheme;
+              # proxy_set_header X-Forwarded-Protocol $scheme;
+              # proxy_set_header X-Forwarded-Host $http_host;
             '';
           };
           "/socket" = {
@@ -397,11 +397,11 @@
               proxy_set_header Upgrade $http_upgrade;
               proxy_set_header Connection "upgrade";
               proxy_set_header Host $host;
-              proxy_set_header X-Real-IP $remote_addr;
-              proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-              proxy_set_header X-Forwarded-Proto $scheme;
-              proxy_set_header X-Forwarded-Protocol $scheme;
-              proxy_set_header X-Forwarded-Host $http_host;
+              # proxy_set_header X-Real-IP $remote_addr;
+              # proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+              # proxy_set_header X-Forwarded-Proto $scheme;
+              # proxy_set_header X-Forwarded-Protocol $scheme;
+              # proxy_set_header X-Forwarded-Host $http_host;
             '';
           };
         };
