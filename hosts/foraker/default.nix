91634956a947195ef378918998822b076bfcc77b
Riley Martin <rileyseanm@gmail.com>
2024-02-07 16:23:13 -0500

    .

--- hosts/foraker/default.nix
+++ hosts/foraker/default.nix
@@ -152,7 +152,7 @@
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
-  services.openssh.PasswordAuthentication = false;
+  services.openssh.settings.PasswordAuthentication = false;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
