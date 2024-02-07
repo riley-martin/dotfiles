202f5fa2cb05b301714c00c698a5005fba9bb98e
Riley Martin <rileyseanm@gmail.com>
2024-02-06 19:45:06 -0500

    .

--- hosts/elias/default.nix
+++ hosts/elias/default.nix
@@ -163,6 +163,22 @@
         };
         extraOptions = ["--cap-add" "MKNOD"];
       };
+      containers.jellyfin = {
+        autoStart = true;
+          image = "jellyfin/jellyfin";
+          volumes = [
+            "/media/Containers/Jellyfin/config:/config"
+            "/media/Containers/Jellyfin/cache:/cache"
+            "/media/Containers/Jellyfin/log:/log"
+            "/media/Movies:/movies"
+            "/media/TV-Series:/tv"
+          ];
+          ports = [ "8096:8096" ];
+          environment = {
+            JELLYFIN_LOG_DIR = "/log";
+          };
+      };
+  
     };
 
 
