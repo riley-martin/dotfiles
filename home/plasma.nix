002fef606c095dddf30fb0c57b472396b07f4a33
Riley Martin <rileyseanm@gmail.com>
2024-03-11 10:14:47 -0400

    .

--- home/plasma.nix
+++ home/plasma.nix
@@ -2,4 +2,5 @@
   services.xserver.enable = true;
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma6.enable = true;
+  programs.kdeconnect.enable = true;
 }
