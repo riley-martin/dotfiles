f7fd89b0dd8cd4f562d8b85509780ec4f1f7cd53
Riley Martin <rileyseanm@gmail.com>
2024-03-15 20:01:26 -0400

    .

--- home/gnome.nix
+++ home/gnome.nix
@@ -1,15 +1,18 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+let
+  packages = with pkgs; [ gnome.gnome-tweaks gnome.gnome-settings-daemon ];
+  extensions = with pkgs.gnomeExtensions; [ tophat window-gestures appindicator ];
+in
+{
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.desktopManager.gnome.sessionPath = with pkgs; [ libgtop ];
   environment.gnome.excludePackages = (with pkgs.gnome; [
     atomix
     hitori
     iagno
     tali
   ]);
-  environment.systemPackages = (
-    ( with pkgs; [ gnome.gnome-tweaks libgtop ] )
-    // ( with pkgs.gnomeExtensions; [ tophat window-gestures] )
-  );
+  environment.systemPackages = packages ++ extensions;
 }
