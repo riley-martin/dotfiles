08b540ebdd29d10e39c90e117d4baf95fc86b693
Riley Martin <rileyseanm@gmail.com>
2024-03-26 09:26:11 -0400

    .

--- system/subsonic.nix
+++ system/subsonic.nix
@@ -2,7 +2,10 @@
   services.gonic = {
     enable = true;
     settings = {
-      music-path = "/home/jellyfin";
+      music-path = "";
+      podcast-path = "";
+      playlists-path = "";
+      cache-path = "/home/subsonic";
     };
   };
   services.nginx.virtualHosts."music.rileymartin.dev" = {
