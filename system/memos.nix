ab381d4243893fd6ae4b217ae24df58067ef71e0
Riley Martin <rileyseanm@gmail.com>
2024-04-19 10:58:35 -0400

    .

---
+++ system/memos.nix
@@ -0,0 +1,28 @@
+{ ... }:
+let
+  version = "3.0";
+in {
+  services.nginx.virtualHosts."memos.rileymartin.dev" = {
+    extraConfig = ''
+      client_max_body_size 50000M;
+    '';
+    forceSSL = true;
+    enableACME = true;
+    locations."/" = {
+      proxyPass = "http://127.0.0.1:5230";
+      proxyWebsockets = true;
+    };
+  };
+
+  virtualisation.oci-containers.containers.memos = {
+    image = "neosmemo/memos:${version}";
+    ports = ["127.0.0.1:5230:5230"];
+    extraOptions = [
+      "--pull=always"
+    ];
+    volumes = [
+      "/home/memos:/var/opt/memos"
+    ];
+  };
+  
+}
