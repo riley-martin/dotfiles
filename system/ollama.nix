0d832b41207b927fb0825e53052d40c88b8daf5f
Riley Martin <rileyseanm@gmail.com>
2024-06-23 14:38:49 -0400

    .

--- system/ollama.nix
+++ system/ollama.nix
@@ -2,7 +2,9 @@
   services.ollama = {
     enable = true;
     acceleration = "rocm";
-    listenAddress = "0.0.0.0:11434";
+    # listenAddress = "0.0.0.0:11434";
+    host = "0.0.0.0";
+    port = "11434";
     environmentVariables = {
       HSA_OVERRIDE_GFX_VERSION = "9.0.0";
     };
