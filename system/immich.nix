b027b9e66b3c572630ccd41c2a16a0e527af7635
Riley Martin <rileyseanm@gmail.com>
2024-07-31 20:51:27 -0400

    .

--- system/immich.nix
+++ system/immich.nix
@@ -6,7 +6,7 @@ let
   immichRoot = "/home/immich"; # TODO: Tweak these to your desired storage locations
   immichPhotos = "${immichRoot}/photos";
   immichAppdataRoot = "${immichRoot}/appdata";
-  immichVersion = "v1.110.0";
+  immichVersion = "v1.111.0";
   immichExternalVolume1 = "/home/riley/Video";
   immichExternalVolume2 = "/home/riley/Picture";
 
