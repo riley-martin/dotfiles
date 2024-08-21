8d281e4f482fe66d3dc5d4995cab19aa774102c7
Riley Martin <rileyseanm@gmail.com>
2024-08-21 17:18:17 -0400

    .

--- system/nginx-foraker.nix
+++ system/nginx-foraker.nix
@@ -44,7 +44,7 @@
             client_body_buffer_size 512k;
             proxy_read_timeout 86400s;
             client_max_body_size 0;
-            proxy_headers_hash_max_size 1024;
+            proxy_headers_hash_max_size 8192;
 
             # Websocket
             proxy_http_version 1.1;
