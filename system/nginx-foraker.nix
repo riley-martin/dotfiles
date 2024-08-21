b224cc1a0811321fccdd32dd29558d13096b816a
Riley Martin <rileyseanm@gmail.com>
2024-08-21 17:50:45 -0400

    .

--- system/nginx-foraker.nix
+++ system/nginx-foraker.nix
@@ -35,6 +35,8 @@
           extraConfig = ''
             proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
             proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header Host $host;
+            proxy_set_header X-Forwarded-Proto $scheme;
     
             client_body_buffer_size 512k;
             proxy_read_timeout 86400s;
