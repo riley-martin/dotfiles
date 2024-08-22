b4736d78f59581c217bea7ff1e982bc570d95d8d
Riley Martin <rileyseanm@gmail.com>
2024-08-21 20:38:51 -0400

    .

--- system/paperless.nix
+++ system/paperless.nix
@@ -3,5 +3,6 @@
     enable = true;
     passwordFile = config.age.secrets.paperless.path;
     address = "100.106.82.60";
+    settings.PAPERLESS_CSRF_TRUSTED_ORIGINS = ["https://papers.rileymartin.dev"];
   };
 }
