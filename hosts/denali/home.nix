52926d4d4d5043478c6a70f525d0ce9bcca2dfd0
Riley Martin <rileyseanm@gmail.com>
2024-09-20 11:04:57 -0400

    .

--- hosts/denali/home.nix
+++ hosts/denali/home.nix
@@ -94,7 +94,7 @@ in {
     gh nil age vscodium android-studio
 
     ## Graphics
-    digikam exiftool darktable freecad blender hugin
+    digikam exiftool darktable blender hugin
     (callPackage ../../pkgs{}).gimp-devel
     # gimp
     kdenlive inkscape audacity
