37e3880c827b67a503eac36c31cb68eca9fbd34a
Riley Martin <rileyseanm@gmail.com>
2024-11-27 11:35:42 -0500

    .

--- system/homeassistant.nix
+++ system/homeassistant.nix
@@ -1,6 +1,9 @@
 { ... }: {
   services.home-assistant = {
-    config = null;
     enable = true;
+    config = {
+      http.use_x_forwarded_for = true;
+      trusted_proxies = ["100.106.82.60" "fd7a:115c:a1e0::4601:523c"];
+    };
   };
 }
