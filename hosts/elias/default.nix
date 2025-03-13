dafd2745309a0154fabe159c07c49bfc03b3a87b
Riley Martin <rileyseanm@gmail.com>
2025-03-13 13:38:16 +0000

    samba

--- hosts/elias/default.nix
+++ hosts/elias/default.nix
@@ -47,6 +47,51 @@
   services.tailscale.enable = true;
   services.tailscale.useRoutingFeatures = "both";
 
+  services.samba = {
+    enable = true;
+    openFirewall = true;
+    settings = {
+      global = {
+        "workgroup" = "WORKGROUP";
+        "server string" = "smbnix";
+        "netbios name" = "smbnix";
+        "security" = "user";
+        #"use sendfile" = "yes";
+        #"max protocol" = "smb2";
+        # note: localhost is the ipv6 localhost ::1
+        "hosts allow" = "192.168.0. 127.0.0.1 localhost";
+        "hosts deny" = "0.0.0.0/0";
+        "guest account" = "nobody";
+        "map to guest" = "bad user";
+      };
+      "video" = {
+        "path" = "/mnt/video";
+        "browseable" = "yes";
+        "read only" = "no";
+        "guest ok" = "yes";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+        "force user" = "riley";
+        "force group" = "users";
+      };
+    };
+  };
+
+  services.samba-wsdd = {
+    enable = true;
+    openFirewall = true;
+  };
+
+  services.avahi = {
+    publish.enable = true;
+    publish.userServices = true;
+    # ^^ Needed to allow samba to automatically register mDNS records (without the need for an `extraServiceFile`
+    nssmdns4 = true;
+    # ^^ Not one hundred percent sure if this is needed- if it aint broke, don't fix it
+    enable = true;
+    openFirewall = true;
+  };
+
   age = {
     # secrets.nextcloud-mail.file = ../../secrets/nextcloud-mail.age;
     secrets.paperless.file = ../../secrets/paperless.age;
